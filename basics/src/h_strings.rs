a3e31ea95025ba4b8f9f4976c9d1d829e6722dad
Webber <webber@takken.io>
2021-08-24 23:45:54 +0200

    strings 2

--- basics/src/h_strings.rs
+++ basics/src/h_strings.rs
@@ -1,7 +1,8 @@
 ﻿pub fn main() {
     println!("\nstrings...");
 
-    string_slice()
+    string_slice();
+    get_nth_character();
 }
 
 #[allow(unused_variables)]
@@ -14,4 +15,16 @@ fn string_slice() {
     let string_from_string_slice_to_string: String = string_literal.to_string();
 
     // A borrowed string slice can not be modified, whereas a string (String) can be modified.
+}
+
+fn get_nth_character() {
+    let string_of_characters: String = String::from("hørses");
+
+    // This can not be done, because non-ascii characters use multiple bytes.
+    // let third_character = string_of_characters[2];
+
+    let third_character = string_of_characters.chars().nth(2).unwrap();
+
+    println!("{} is the third character in {}", third_character, string_of_characters);
+    // prints "r is the third character in hørses"
 }
