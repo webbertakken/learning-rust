95220d40f30ed691b0d993b1197fee54cf227cda
Webber <webber@takken.io>
2021-09-02 01:00:09 +0200

    remove obsolete comment

--- image-processing/src/main.rs
+++ image-processing/src/main.rs
@@ -207,21 +207,3 @@ fn fractal(image: DynamicImage) -> DynamicImage {
 
     return new_image;
 }
-
-// **SUPER CHALLENGE FOR LATER** - Let's face it, you don't have time for this during class.
-//
-// Make all of the subcommands stackable!
-//
-// For example, if you run:
-//
-//   cargo run infile.png outfile.png blur 2.5 invert rotate 180 brighten 10
-//
-// ...then your program would:
-// - read infile.png
-// - apply a blur of 2.5
-// - invert the colors
-// - rotate the image 180 degrees clockwise
-// - brighten the image by 10
-// - and write the result to outfile.png
-//
-// Good luck!
