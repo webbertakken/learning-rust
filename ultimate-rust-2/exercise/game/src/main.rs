f73ad996db1f5eff6d1ba987a3625fa6acbb6bc1
Webber <webber@takken.io>
2022-10-15 18:56:47 +0200

    feat: setup game project

---
+++ ultimate-rust-2/exercise/game/src/main.rs
@@ -0,0 +1,33 @@
+﻿use rusty_engine::prelude::*;
+
+struct Player {}
+
+struct GameState {
+    player: Player,
+    score: u32,
+    enemy_labels: Vec<String>,
+    spawn_timer: Timer,
+}
+
+impl Default for GameState {
+    fn default() -> Self {
+        Self {
+            player: Player {},
+            score: 0,
+            enemy_labels: Vec::new(),
+            spawn_timer: Timer::from_seconds(1.0, false),
+        }
+    }
+}
+
+fn main() {
+    let mut game = Game::new();
+
+    game.add_logic(game_logic);
+    game.run(GameState::default());
+}
+
+fn game_logic(engine: &mut Engine, game_state: &mut GameState) {
+    game_state.score += 1;
+    println!("current score: {}", game_state.score);
+}
